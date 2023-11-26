a5fc224561434d3ba207a0e6a5597a7a2f9ea816
garrit523 <lichtenberggj@vcu.edu>
2023-11-26 10:39:45 -0500

    image ul gallery on post template

--- site/templates/post.php
+++ site/templates/post.php
@@ -3,6 +3,18 @@
         <h1><i><?= $page->title()->esc() ?></i></h1>
         <h4><?= $page->date()->esc() ?></h4>
 
+        <div>
+            <ul>
+                <?php foreach ($page->images() as $image): ?>
+                    <li>
+                        <a href="<?= $image->url() ?>">
+                            <img style="margin-top: 14pt;" class="img" src="<?= $image->url() ?>">
+                        </a>
+                    </li>
+                <?php endforeach ?>
+            </ul>
+        </div>
+
         <span>
         <?php if ($cover = $page->cover()): ?>
         <a href="<?= $cover->url() ?>">
