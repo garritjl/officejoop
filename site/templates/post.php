b7356568db0261b3e7ff1278999b8fb70dbb4806
garrit523 <lichtenberggj@vcu.edu>
2023-11-26 10:48:23 -0500

    max width of div

--- site/templates/post.php
+++ site/templates/post.php
@@ -3,7 +3,7 @@
         <h1><i><?= $page->title()->esc() ?></i></h1>
         <h4><?= $page->date()->esc() ?></h4>
 
-        <div>
+        <div class="gallery">
             <ul>
                 <?php foreach ($page->images() as $image): ?>
                     <li>
@@ -14,22 +14,6 @@
                 <?php endforeach ?>
             </ul>
         </div>
-
-        <span>
-        <?php if ($cover = $page->cover()): ?>
-        <a href="<?= $cover->url() ?>">
-            <img style="margin-top: 14pt;" class="img" src="<?= $cover->url() ?>">
-        </a>
-        <?php endif ?>
-        </span>
-<br>
-        <span>
-        <?php if ($image = $page->image()): ?>
-        <a href="<?= $image->url() ?>">
-            <img style="margin-top: 14pt;" class="img" src="<?= $image->url() ?>" alt="none">
-        </a>
-        <?php endif ?>
-        </span>
 
         <p><?= $page->description()->esc() ?></p>
     </article>
