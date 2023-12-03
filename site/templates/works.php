31795c11492491f7752251d322c555bd6c59c5fc
garrit523 <lichtenberggj@vcu.edu>
2023-12-03 09:11:59 -0500

    adding date to listing

--- site/templates/works.php
+++ site/templates/works.php
@@ -3,7 +3,7 @@
         <h1><?= $page->title()->esc() ?></h1>
         
         <ul>
-            <?php foreach ($page->children()->listed() as $item): ?>
+            <?php foreach ($page->children()->listed()->flip() as $item): ?>
             <a class="green" <?php e($item->isOpen(), 'aria-current="page"') ?> href="<?= $item->url() ?>"><li><?= $item->title()->esc() ?></li></a>
             <?php endforeach ?>
         </ul>
